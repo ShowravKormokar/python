d041ae3f95c7c3d0a002673e4e199110c8387432
ShowravKormokar <showravkormokar354@gmail.com>
2025-07-23 22:05:05 +0600

    Declaration variables in different ways

---
+++ Variables/declare.py
@@ -0,0 +1,27 @@
+# Declaration variable
+id =77
+name = "Showrav"
+section = 'A'
+
+print(id , name ,section)
+
+#Multiple variable declaration
+a,b,c = "A", "B", "C"
+
+print(a)
+print(b)
+print(c)
+
+#Unpacked collection
+print("Unpacked Collection->")
+fruits=["Mango","Banana","Orange"]
+f1,f2,f3 = fruits
+print(f1)
+print(f2)
+print(f3)
+
+# Concat. var
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
