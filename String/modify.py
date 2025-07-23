1461048ead144709623aa1c2418595ffb876225a
ShowravKormokar <showravkormokar354@gmail.com>
2025-07-23 23:02:39 +0600

    Perfrom diff. types of modification methods on string

---
+++ String/modify.py
@@ -0,0 +1,18 @@
+# Upper/Lower case conversion
+str = "Showrav"
+
+print(str.upper()) #Uppercase
+print(str.lower()) #Lowercase
+
+#Remove Whitespaces
+place = " Rajshshi"
+print(place.strip())
+
+# Replace string
+print(place.replace("hs","ha"))
+
+# Split string
+msg = "How are you?"
+print(msg.split(", "))
+
+print(str.islower())
