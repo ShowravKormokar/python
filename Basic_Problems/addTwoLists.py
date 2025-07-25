bd762ac1a0f56dfed03bff04be3bb55ec5c9084a
ShowravKormokar <showravkormokar354@gmail.com>
2025-07-25 23:58:47 +0600

    Add two list elements together

---
+++ Basic_Problems/addTwoLists.py
@@ -0,0 +1,9 @@
+# Adding two list elements together
+list1 = ['s','h','o','w','r','a','v']
+list2 = ['s','h','o','w','r','a','v']
+
+res_List = []
+for i in range(0, len(list1)):
+    res_List.append(list1[i]+list2[i])
+
+print(res_List)
